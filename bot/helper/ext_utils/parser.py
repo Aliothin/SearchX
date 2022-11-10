70aff24823520fe1214dde456419c4eec52fae8a
majnurangeela <84721324+majnurangeela@users.noreply.github.com>
2022-11-11 00:25:11 +0530

    Updated Bypass Code

--- bot/helper/ext_utils/parser.py
+++ bot/helper/ext_utils/parser.py
@@ -1,6 +1,7 @@
 import base64
 import re
 import requests
+import cloudscraper
 
 from lxml import etree
 from urllib.parse import urlparse, parse_qs
@@ -39,7 +40,7 @@ def parse_infou(data):
 def unified(url: str) -> str:
     if (UNIFIED_EMAIL or UNIFIED_PASS) is None:
         raise DDLException("UNIFIED_EMAIL and UNIFIED_PASS env vars not provided")
-    client = requests.Session()
+    client = cloudscraper.create_scraper(delay=10, browser='chrome')
     client.headers.update({
         "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36"
     })
@@ -69,123 +70,135 @@ def unified(url: str) -> str:
         info_parsed['link_type'] = 'direct'
         data['action'] = 'direct'
     
-    while data['type'] <= 3:
+    while data["type"] <= 3:
         try:
             response = client.post(url, data=gen_payload(data), headers=headers).json()
             break
-        except: data['type'] += 1
+        except:
+            data["type"] += 1
         
-    if 'url' in response:
-        info_parsed['gdrive_link'] = response['url']
-    elif 'error' in response and response['error']:
-        info_parsed['error'] = True
-        info_parsed['error_message'] = response['message']
+    if "url" in response:
+        info_parsed["gdrive_link"] = response["url"]
+    elif "error" in response and response["error"]:
+        info_parsed["error"] = True
+        info_parsed["error_message"] = response["message"]
     else:
-        info_parsed['error'] = True
-        info_parsed['error_message'] = 'Something went wrong :('
+        info_parsed["error"] = True
+        info_parsed["error_message"] = "Something went wrong :("
+        
     
-    if urlparse(url).netloc == 'appdrive.in' and not info_parsed['error']:
-        flink = info_parsed['gdrive_link']
+    if info_parsed["error"]:
+        raise DDLException(f"ERROR! {info_parsed['error_message']}")
+    
+    
+    if urlparse(url).netloc == "appdrive.info":
+        flink = info_parsed["gdrive_link"]
         return flink
-    
-    if urlparse(url).netloc == 'driveapp.in' and not info_parsed['error']:
-        res = client.get(info_parsed['gdrive_link'])
+
+    elif urlparse(url).netloc == "driveapp.in":
+        res = client.get(info_parsed["gdrive_link"])
         drive_link = etree.HTML(res.content).xpath("//a[contains(@class,'btn')]/@href")[0]
         flink = drive_link
+        return flink
 
-    if urlparse(url).netloc == 'drivesharer.in' and not info_parsed['error']:
-        res = client.get(info_parsed['gdrive_link'])
+    else:
+        res = client.get(info_parsed["gdrive_link"])
         drive_link = etree.HTML(res.content).xpath("//a[contains(@class,'btn btn-primary')]/@href")[0]
         flink = drive_link
-
-    if urlparse(url).netloc == 'drivebit.in' and not info_parsed['error']:
-        res = client.get(info_parsed['gdrive_link'])
-        drive_link = etree.HTML(res.content).xpath("//a[contains(@class,'btn btn-primary')]/@href")[0]
-        flink = drive_link
-        
-    if urlparse(url).netloc == 'drivelinks.in' and not info_parsed['error']:
-        res = client.get(info_parsed['gdrive_link'])
-        drive_link = etree.HTML(res.content).xpath("//a[contains(@class,'btn btn-primary')]/@href")[0]
-        flink = drive_link
-
-
-    info_parsed['src_url'] = url
-    
-    return flink
+        return flink
 
                            
-def parse_info(res):
+def parse_info(res, url):
     info_parsed = {}
-    title = re.findall('>(.*?)<\/h4>', res.text)[0]
-    info_chunks = re.findall('>(.*?)<\/td>', res.text)
-    info_parsed['title'] = title
+    if 'drivebuzz' in url:
+        info_chunks = re.findall('<td\salign="right">(.*?)<\/td>', res.text)
+    else:
+        info_chunks = re.findall(">(.*?)<\/td>", res.text)
     for i in range(0, len(info_chunks), 2):
-        info_parsed[info_chunks[i]] = info_chunks[i+1]
+        info_parsed[info_chunks[i]] = info_chunks[i + 1]
     return info_parsed
 
 def udrive(url: str) -> str:
-    client = requests.Session()
-    if 'hubdrive' in url:
-        client.cookies.update({'crypt': HUBDRIVE_CRYPT})
-    if 'drivehub' in url:
-        client.cookies.update({'crypt': HUBDRIVE_CRYPT})
-    if 'katdrive' in url:
-        client.cookies.update({'crypt': KATDRIVE_CRYPT})
-    if 'kolop' in url:
-        client.cookies.update({'crypt': KATDRIVE_CRYPT})
-    if 'drivefire' in url:
-        client.cookies.update({'crypt': DRIVEFIRE_CRYPT})
+    if 'katdrive' or 'hubdrive' in url:
+        client = requests.Session()
+    else:
+        client = cloudscraper.create_scraper(delay=10, browser='chrome')
+        
+    if "hubdrive" in url:
+        if "hubdrive.in" in url:
+            url = url.replace(".in",".pro")
+        client.cookies.update({"crypt": HUBDRIVE_CRYPT})
+    if "drivehub" in url:
+        client.cookies.update({"crypt": KATDRIVE_CRYPT})
+    if "katdrive" in url:
+        client.cookies.update({"crypt": KATDRIVE_CRYPT})
+    if "kolop" in url:
+        client.cookies.update({"crypt": KATDRIVE_CRYPT})
+    if "drivefire" in url:
+        client.cookies.update({"crypt": DRIVEFIRE_CRYPT})
+    if "drivebuzz" in url:
+        client.cookies.update({"crypt": DRIVEFIRE_CRYPT})
+        
     res = client.get(url)
-    info_parsed = parse_info(res)
-    info_parsed['error'] = False
-    
+    info_parsed = parse_info(res, url)
+    info_parsed["error"] = False
+
     up = urlparse(url)
     req_url = f"{up.scheme}://{up.netloc}/ajax.php?ajax=download"
-    
-    file_id = url.split('/')[-1]
-    
-    data = { 'id': file_id }
-    
-    headers = {
-        'x-requested-with': 'XMLHttpRequest'
-    }
-    
-    try:
-        res = client.post(req_url, headers=headers, data=data).json()['file']
-    except: return {'error': True, 'src_url': url}
-    
-    if 'drivefire.co' in url:
-      return res
-    else:
-      gd_id = re.findall('gd=(.*)', res, re.DOTALL)[0]
-    
-    info_parsed['gdrive_url'] = f"https://drive.google.com/open?id={gd_id}"
-    info_parsed['src_url'] = url
 
-    return info_parsed['gdrive_url']        
+    file_id = url.split("/")[-1]
+
+    data = {"id": file_id}
+
+    headers = {"x-requested-with": "XMLHttpRequest"}
+
+    try:
+        res = client.post(req_url, headers=headers, data=data).json()["file"]
+    except:
+        raise DDLException("ERROR! File Not Found or User rate exceeded !!")
+    
+    if 'drivefire' in url:
+        decoded_id = res.rsplit('/', 1)[-1]
+        flink = f"https://drive.google.com/file/d/{decoded_id}"
+        return flink
+    elif 'drivehub' in url:
+        gd_id = res.rsplit("=", 1)[-1]
+        flink = f"https://drive.google.com/open?id={gd_id}"
+        return flink
+    elif 'drivebuzz' in url:
+        gd_id = res.rsplit("=", 1)[-1]
+        flink = f"https://drive.google.com/open?id={gd_id}"
+        return flink
+    else:
+        gd_id = re.findall('gd=(.*)', res, re.DOTALL)[0]
+        info_parsed["gdrive_url"] = f"https://drive.google.com/open?id={gd_id}"
+        info_parsed["src_url"] = url
+        flink = info_parsed['gdrive_url']
+        return flink        
         
 def gdtot(url: str) -> str:
     if GDTOT_CRYPT is None:
         raise DDLException("GDTOT_CRYPT env var not provided")
     client = requests.Session()
-    client.cookies.update({'crypt': GDTOT_CRYPT})
+    client.cookies.update({"crypt": GDTOT_CRYPT})
     res = client.get(url)
-    res = client.get(f"https://new.gdtot.nl/dld?id={url.split('/')[-1]}")
+    base_url = re.match('^.+?[^\/:](?=[?\/]|$\n)', url).group(0)
+    res = client.get(f"{base_url}/dld?id={url.split('/')[-1]}")
     url = re.findall(r'URL=(.*?)"', res.text)[0]
     info = {}
-    info['error'] = False
+    info["error"] = False
     params = parse_qs(urlparse(url).query)
-    if 'gd' not in params or not params['gd'] or params['gd'][0] == 'false':
-        info['error'] = True
-        if 'msgx' in params:
-            info['message'] = params['msgx'][0]
+    if "gd" not in params or not params["gd"] or params["gd"][0] == "false":
+        info["error"] = True
+        if "msgx" in params:
+            info["message"] = params["msgx"][0]
         else:
-            info['message'] = 'Invalid link'
+            info["message"] = "Invalid link"
     else:
-        decoded_id = base64.b64decode(str(params['gd'][0])).decode('utf-8')
-        drive_link = f'https://drive.google.com/open?id={decoded_id}'
-        info['gdrive_link'] = drive_link
-    if not info['error']:
-        return info['gdrive_link']
+        decoded_id = base64.b64decode(str(params["gd"][0])).decode("utf-8")
+        drive_link = f"https://drive.google.com/open?id={decoded_id}"
+        info["gdrive_link"] = drive_link
+    if not info["error"]:
+        return info["gdrive_link"]
     else:
         raise DDLException(f"{info['message']}")
